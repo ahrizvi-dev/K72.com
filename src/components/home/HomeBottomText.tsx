d0d541e548d8f2c86e01bc06e081db22d04d1791
Muhammad Ali Hamza <ahrizvi.dev@gmail.com>
2025-09-05 22:21:38 +0500

    Upload Vercel Again

--- src/components/home/HomeBottomText.tsx
+++ src/components/home/HomeBottomText.tsx
@@ -19,11 +19,11 @@ const HomeBottomText = () => {
   }, []);
 
   return (
-    <div className="w-full flex flex-col flex-col-reverse  md:flex md:items-end  md:gap-[15rem]">
+    <div className="w-full flex flex-col-reverse  md:flex md:flex-row md:items-end  md:gap-[15rem]">
       <div className="flex justify-start items-center pl-4 gap-2 text-white py-4  md:pb-4">
         <Globe /> {time} 2025 Pakistan
       </div>
-      <div className="text-white flex justify-center gap-8 md:gap-12 ">
+      <div className="text-white flex justify-center gap-8 md:gap-12 pb-4">
         <Link
           to={"/projects"}
           className="text-[4vw] font-[font500] border-2 border-white px-8 rounded-full uppercase leading-tight">
